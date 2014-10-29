b08e4d3bbffbdef0ce07540ba16b505cd7b5596d
Mohsin Inayat <mohsin.inayat@gmail.com>
2014-10-29 16:09:36 +0400

    Blacklist Module

---
+++ app/views/pbxmanagement/extra_telephone_features/black_list_form.blade.php
@@ -0,0 +1,42 @@
+@section('form_title')
+	<h2 class="what">Edit Blacklist</h2>
+@stop
+
+@section('form_content')
+	
+	<section class="tab_wrap no_tab">
+	
+		<div class="tab_content_pad active">
+			
+			@include('layout.sfield_open')
+				
+				<!-- Callgroup Form -->
+				<form name="frmBlacklist" id="frmBlacklist">
+				<input type="hidden" name="blacklist_id" id="blacklist_id" value="" />
+				<div class="col_wide">
+					<?php 
+					$extension_options = array('ALL' => 'ALL');
+					foreach($data['extensions'] as $extension) {
+						$extension_options[$extension['account_id']] = $extension['name'].' - '.$extension['extennumber'];
+					}
+					?>
+					@include('layout.sfield_generator', array('wrap' => 'crow', 'type' => 'select', 'label' => 'Extension', 'id' => 'exten', 'options' => $extension_options))
+					@include('layout.sfield_generator', array('wrap' => 'crow', 'type' => 'text', 'label' => 'Blacklist Number', 'id' => 'phonenumber'))
+				</div>
+				
+				
+			@include('layout.sfield_close')
+				
+			<span class="button_wrap topspace">
+				<input id="sbtBtn" name="sbtBtn" type="button" value="Save">
+				<input type="button" value="Cancel" class="gray" onclick="hidePopup()">
+			</span>
+			</form>
+			<!-- End of Callgroup Form -->
+			
+			<div class="clearfix"></div>
+		</div>
+		
+	</section>
+	
+@stop
